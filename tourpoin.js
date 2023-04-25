9e5d9f292aa19c16bd8d3cf186440ee0dec36c05
gunlapukha <124670796+gunlapukha@users.noreply.github.com>
2023-04-25 16:01:55 +0700

    Add files via upload

---
+++ tourpoin.js
@@ -0,0 +1,155 @@
+var tourpoint = {
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "threepagodaspass",
+          "type": "culture",
+          "level": 10
+        },
+        "geometry": {
+          "coordinates": [
+            98.38684640638667,
+            15.3019372471244
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "mueangsinghistoricalpark",
+          "type": "culture",
+          "level": 8
+        },
+        "geometry": {
+          "coordinates": [
+            99.24215672357826,
+            14.041464629294012
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "erawanwaterfall ",
+          "type": "nature",
+          "level": 7
+        },
+        "geometry": {
+          "coordinates": [
+            99.14231867470471,
+            14.360420947992708
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "vajiralongkorndam",
+          "type": "nature",
+          "level": 8
+        },
+        "geometry": {
+          "coordinates": [
+            98.59645940638529,
+            14.789697748186313
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "saiyoknoiwaterfall",
+          "type": "nature",
+          "level": 10
+        },
+        "geometry": {
+          "coordinates": [
+            99.05834294302906,
+            14.238036063045953
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "type": "nature",
+          "name": "jogkradinWaterfall",
+          "level": 9
+        },
+        "geometry": {
+          "coordinates": [
+            98.38322101631746,
+            14.696067837970432
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "giantmonkypodtree",
+          "type": "nature",
+          "level": 6
+        },
+        "geometry": {
+          "coordinates": [
+            99.52742321135196,
+            13.955118869037904
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "thathungnadam",
+          "type": "nature",
+          "level": 7
+        },
+        "geometry": {
+          "coordinates": [
+            99.23771074799947,
+            14.23176006799325
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "chateaudesangkhlaburi",
+          "type": "culture",
+          "level": 10
+        },
+        "geometry": {
+          "coordinates": [
+            98.45473440638244,
+            15.149953321404041
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "watthamsua",
+          "type": "culture",
+          "level": 9
+        },
+        "geometry": {
+          "coordinates": [
+            99.60527217470968,
+            13.953703370069135
+          ],
+          "type": "Point"
+        }
+      }
+    ]
+  }
